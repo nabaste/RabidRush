080f4369ffc0440c4e423cc767adfdd1ff3656d4
Nahuel <nbaste@gmail.com>
2022-03-03 01:13:36 -0300

    Skyboxes and visual references

--- Assets/Scripts/UI/SideMenu.cs
+++ Assets/Scripts/UI/SideMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine.UIElements;
 public abstract class SideMenu : MonoBehaviour
 {
     protected VisualElement Root;
+
     private void OnEnable()
     {
         Root = GetComponentInParent<UIDocument>().rootVisualElement;
